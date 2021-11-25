36776c6c5cc452c595d79b62eec9c8ef1b6a723c
huy-axie <huy@axieinfinity.com>
2021-11-25 18:28:55 +0700

    add splitMessage

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -63,23 +64,32 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
+	// Check message & send
 	if strings.HasPrefix(m.Content, "flarectl") {
+
 		s.ChannelMessageSend(m.ChannelID, "executing... ```"+m.Content+"```")
-		stdout, err := execShell(m.Content)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "```"+err.Error()+"```")
-			return
+		stdout, _ := execShell(m.Content)
+		// slpit & send
+		if len(stdout) > 1950 {
+			res := splitMessage(stdout, 1950)
+			for _, msg := range res {
+				s.ChannelMessageSend(m.ChannelID, "```"+msg+"```")
+				time.Sleep(2 * time.Second)
+			}
+		} else {
+			// Just send normaly
+			s.ChannelMessageSend(m.ChannelID, "```"+stdout+"```")
 		}
-		s.ChannelMessageSend(m.ChannelID, stdout)
 	}
 
 	// If the message is "Ping" reply with "Pong!"
 	if m.Content == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
+
 }
 
+// exec bash & catch output
 func execShell(command string) (string, error) {
 
 	cmd := exec.Command("bash", "-c", command)
@@ -95,9 +105,34 @@ func execShell(command string) (string, error) {
 	outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
 
 	if len(outStr) > 0 {
+		println(len(outStr))
 		return outStr, nil
 	} else {
+		println(len(errStr))
 		return errStr, nil
 	}
 
+}
+
+// split message by discord limit 2000 character
+func splitMessage(msg string, chunkSize int) []string {
+	if len(msg) == 0 {
+		return nil
+	}
+	if chunkSize >= len(msg) {
+		return []string{msg}
+	}
+	var chunks []string = make([]string, 0, (len(msg)-1)/chunkSize+1)
+	currentLen := 0
+	currentStart := 0
+	for i := range msg {
+		if currentLen == chunkSize {
+			chunks = append(chunks, msg[currentStart:i])
+			currentLen = 0
+			currentStart = i
+		}
+		currentLen++
+	}
+	chunks = append(chunks, msg[currentStart:])
+	return chunks
 }
